ccb3d57b66405a34e2e75382d8307532d23a9082
dikky <awandikky@gmail.com>
2018-11-12 18:52:03 +0700

    dikky

--- uas_coba/app/src/main/java/com/example/k/uas_coba/Beranda.java
+++ uas_coba/app/src/main/java/com/example/k/uas_coba/Beranda.java
@@ -1,13 +1,53 @@
 package com.example.k.uas_coba;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
 
-public class Beranda extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class Beranda extends AppCompatActivity
+{
+    private ArrayList<Integer> mImagesUrl = new ArrayList<>();
+    private ArrayList<Event> mUser = new ArrayList<>();
+
+    private RecyclerView.Adapter madapter;
+    private RecyclerView.LayoutManager laymanager;
+
+    ImageView img_event;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beranda);
+
+        getImages();
+        initRecyclerview();
+
+        img_event = (ImageView) findViewById(R.id.img_itemEvent);
     }
+
+    private void getImages()
+    {
+        mImagesUrl.add(R.drawable.i_user);
+        mImagesUrl.add(R.drawable.i_user);
+        mImagesUrl.add(R.drawable.i_user);
+    }
+
+    private void  initRecyclerview()
+    {
+        laymanager = new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false);
+        RecyclerView recyclerViewww = findViewById(R.id.beranda_rcyEvent);
+        recyclerViewww.setLayoutManager(laymanager);
+
+        madapter = new event_adapter(mImagesUrl,this);
+        recyclerViewww.setAdapter(madapter);
+
+    }
+
 }
--- uas_coba/app/src/main/java/com/example/k/uas_coba/Daftar.java
+++ uas_coba/app/src/main/java/com/example/k/uas_coba/Daftar.java
@@ -9,7 +9,5 @@ public class Daftar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daftar);
-
-
     }
 }
